6c51c010555d7bd9c0a6da615a112ebd3daa6777
Ahmet Hakan ÇELİK <ahmethcelik@gmail.com>
2020-08-05 00:50:40 +0300

    regex added

--- esp32.py
+++ esp32.py
@@ -1,6 +1,7 @@
 # Easy way to read ESP32 MAC Address with python
 # @github: AHakan
 import os
+import re
 
 # Run esptool code
 # You can change the port
@@ -14,6 +15,11 @@ output = stream.read()
 x = output.find("MAC: ")
 
 # Read MAC address
-# And write upper case
 mac = output[x+5:x+22]
-print(mac.upper())
+
+# Regex for mac address
+p = re.compile(r'(?:[0-9a-fA-F]:?){12}')
+
+# re.findall, find mac address
+# and return upper case
+return re.findall(p, mac)[0].upper()
